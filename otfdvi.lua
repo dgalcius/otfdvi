6837b8c1d91fdeb116bb8255fee82d94122575ae
Deimantas Galcius <deimantas.galcius@gmmail.com>
2018-01-14 22:12:59 +0200

    update otflua

--- otfdvi.lua
+++ otfdvi.lua
@@ -9,6 +9,18 @@ local filein  = arg[1] or 'sample2e.dvi'
 local fileout = 'out.dvi'
 local fhi = assert(io.open(filein, 'rb'))
 local fho = assert(io.open(fileout, 'wb'))
-content = dvi.parse(fhi)
-dvi.dump(fho, content)
+local content = dvi.parse(fhi)
+local dvimodified = {}
+
+for _, op  in ipairs(content) do
+   if op._opcode == "pre" then
+   --   print(inspect(op))
+      op.comment = "otfdvi output " .. os.date("%Y.%m.%d:%H%M%S")
+   end
+
+   table.insert(dvimodified, op) 
+end
+
+
+dvi.dump(fho, dvimodified)
 
