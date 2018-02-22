4103272f6ffa06e0c58754e330ab13548519cd1b
Deimantas Galcius <deimantas.galcius@gmmail.com>
2018-02-22 22:38:27 +0200

    clean otfdvi.conf.lua

--- otfdvi.conf.lua
+++ otfdvi.conf.lua
@@ -1,10 +1,4 @@
 return {
-   mkfile_out = "__Makefile",
    mkfile_tmpl = "otfdvi.tmpl.Makefile",
    lua_font_dir = kpse.expand_var("$TEXMFSYSVAR").."/luatex-cache/generic/fonts/otl/",
-   fileout = 'out.dvi',
-   psmapfile = 'ttfonts.map',
-   fontprefix = "font",
-   --   fontprefix = math.rand() .. font
-   htf = false ,
 }
